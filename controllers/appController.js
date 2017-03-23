16c9b3826e8d3aecb7c66d80a48717588a01eeae
Lina25 <lina.kurylo@gmail.com>
2017-03-23 03:05:42 +0300

    add view elements

--- controllers/appController.js
+++ controllers/appController.js
@@ -2,7 +2,12 @@ function AppController(dataService){
 	var scope = this;
 	// setting view elements
 	scope.header = document.getElementById("header");
-
+    scope.cash = document.getElementById("cash").value;
+    scope.res = document.getElementById("result");
+    scope.cash_from = document.getElementById("from").value;
+    scope.cash_to = document.getElementById("to").value;
+    scope.calculate = document.getElementById("calculate");
+  
 
 	// handlers
 	scope.onDataLoadedSuccess = function(result){
@@ -10,15 +15,16 @@ function AppController(dataService){
 	};
 
 	scope.onCalculateHandler = function(){
-
-	};
+//      alert(document.getElementById("cash").value);
+       document.getElementById("result").innerHTML = document.getElementById("cash").value;
+    }
 
 	// constuctor
 	var init = function(){
 		dataService.getAll(scope.onDataLoadedSuccess);
 
 		// event listeners
-		scope.submitBtn.addEventListener('click', scope.onCalculateHandler);
+		scope.calculate.addEventListener('click', scope.onCalculateHandler);
 	};
 
 	init();
