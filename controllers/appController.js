d3b3f1a94ebf62cc5576245bf4355291fab1c8b9
Lina25 <lina.kurylo@gmail.com>
2017-03-25 21:42:52 +0300

    add option data

--- controllers/appController.js
+++ controllers/appController.js
@@ -1,7 +1,7 @@
 function AppController(dataService) {
   var scope = this;
-  
-  
+
+
   // setting view elements
   scope.header = document.getElementById("header");
   scope.cash = document.getElementById("cash");
@@ -9,28 +9,71 @@ function AppController(dataService) {
   scope.cash_from = document.getElementById("from");
   scope.cash_to = document.getElementById("to");
   scope.calculate = document.getElementById("calculate");
-  
-  
+
+
   // handlers
-  scope.onDataLoadedSuccess = function (result) {};
+  scope.onDataLoadedSuccess = function (result) {
+  for (var i = 0; i < result.length; i++) {
+    var a = result[i].txt;
+  }
+
+  var msgContainer = document.createDocumentFragment();
+  for (var i = 0; i < result.length; i++) {
+    var option = msgContainer.appendChild(document.createElement("option"));
+    option.text = result[i].txt;
+  }
+  scope.cash_from.appendChild(msgContainer);
+};
+  
+  scope.onDataLoadedSuccess1 = function (result) {
+  for (var i = 0; i < result.length; i++) {
+    var a = result[i].txt;
+  }
+
+  var msgContainer = document.createDocumentFragment();
+  for (var i = 0; i < result.length; i++) {
+    var option = msgContainer.appendChild(document.createElement("option"));
+    option.text = result[i].txt;
+  }
+  scope.cash_to.appendChild(msgContainer);
+};
+  
+  
   scope.onCalculateHandler = function () {
+  
+    var last_result, vfrom, vto, vcash;
+    vcash = document.getElementById("cash").value;
+    vfrom = document.getElementById("from").value;
+    vto = document.getElementById("to").value;
+    
+                        
     //alert(scope.cash.value);
     //var x = document.getElementById("cash").value;
     //document.getElementById("res").innerHTML = x;
+
   }
   scope.preventDefaultHandler = function (event) {
     event.preventDefault();
   };
-  
-  
+
+
   // constuctor
   var init = function () {
     dataService.getAll(scope.onDataLoadedSuccess);
+    dataService.getAll(scope.onDataLoadedSuccess1);
     // event listeners
-    
-    
+
+
     scope.calculate.addEventListener('click', scope.onCalculateHandler);
     scope.calculate.addEventListener('click', scope.preventDefaultHandler);
   };
   init();
 };
+
+
+
+
+//function myFunction() {
+//    var btn = document.createElement("BUTTON");
+//    document.body.appendChild(btn);
+//}
